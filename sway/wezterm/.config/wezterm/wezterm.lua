46f2646da7d748720911beba2238a9a1e74c5e42
sailorfe <sailorfe@proton.me>
2025-09-08 18:46:20 -0400

    update

--- sway/wezterm/.config/wezterm/wezterm.lua
+++ sway/wezterm/.config/wezterm/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font_size = 13
-config.font = wezterm.font('IBM 3270')
-config.color_scheme = 'Moon Queen'
+config.font_size = 11
+--config.font = wezterm.font('IBM 3270')
+config.color_scheme = 'Perona'
 config.use_fancy_tab_bar = false
 config.window_background_opacity = 1.0
 config.initial_cols = 120
