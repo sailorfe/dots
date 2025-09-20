232e020c607a1f8c554881681e4a2eecb5893000
sailorfe <sailorfe@proton.me>
2025-09-20 12:05:39 -0400

    update desktop sway

--- sway/wezterm/.config/wezterm/wezterm.lua
+++ sway/wezterm/.config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font_size = 11
---config.font = wezterm.font('IBM 3270')
-config.color_scheme = 'Perona'
+config.font = wezterm.font('Rec Mono Casual')
+config.color_scheme = 'Moon Queen'
 config.use_fancy_tab_bar = false
 config.window_background_opacity = 1.0
 config.initial_cols = 120
