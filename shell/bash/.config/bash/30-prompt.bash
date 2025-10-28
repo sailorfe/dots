727df2b7af1aa4e6992c1b3bd5e79a09a7185f0a
sailorfe <sailorfe@proton.me>
2025-10-28 00:53:40 -0400

    update bash prompt

--- shell/bash/.config/bash/30-prompt.bash
+++ shell/bash/.config/bash/30-prompt.bash
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-BG_HOST=$(theme_color black)
-BG_USER=$(theme_color bright_black)
-BG_DIR=$(theme_color magenta)
-FG_HOST=$(theme_color fg)
-FG_DIR=$(theme_color bg)
-FG_VENV=$(theme_color bright_magenta)
+FG_DIR=$(theme_color bright_magenta)
+FG_VENV=$(theme_color bright_cyan)
 FG_GIT=$(theme_color white)
-FG_GLYPH=$(theme_color cyan)
+FG_GLYPH=$(theme_color magenta)
 
 venv_prompt() {
   if [[ -n "$VIRTUAL_ENV" ]]; then
-		echo " $(color_fg $FG_VENV)($(basename "$VIRTUAL_ENV")) $(color_reset)"
+		echo "$(color_fg $FG_VENV)($(basename "$VIRTUAL_ENV"))$(color_reset) "
   fi
 }
 
@@ -20,17 +16,15 @@ git_info() {
   branch=$(git symbolic-ref --short HEAD 2>/dev/null) || return
   git_status=$(git status --porcelain 2>/dev/null)
   [[ -n $git_status ]] && symbol="*" || symbol=""
-  echo -n "$(color_fg $FG_GIT)$branch$symbol$(color_reset)"
+  echo -n " $(color_fg $FG_GIT)$branch$symbol$(color_reset)"
 }
 
 set_prompt() {
   PS1="\n"
 	PS1+=$(venv_prompt)
-	PS1+=$(color_bg $BG_HOST)$(color_fg $FG_HOST)" $(hostname) "$(color_reset)
-  PS1+=$(color_bg $BG_USER)$(color_fg $FG_HOST)" \u "$(color_reset)
-  PS1+=$(color_bg $BG_DIR)$(color_fg $FG_DIR)" \w "$(color_reset)
-  PS1+=" $(git_info) "
-  PS1+=$(color_fg $FG_GLYPH)"$ "$(color_reset)
+  PS1+=$(color_fg $FG_DIR)"\w"$(color_reset)
+  PS1+=$(git_info)
+  PS1+=$(color_fg $FG_GLYPH)" $ "$(color_reset)
 }
 
 PROMPT_COMMAND=set_prompt
