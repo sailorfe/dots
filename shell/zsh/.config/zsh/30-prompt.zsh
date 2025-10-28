a4f024f72c466744664b8d3329ef24200443f742
sailorfe <sailorfe@proton.me>
2025-10-28 04:02:52 +0000

    shorter zsh prompt

--- shell/zsh/.config/zsh/30-prompt.zsh
+++ shell/zsh/.config/zsh/30-prompt.zsh
@@ -4,13 +4,10 @@
 #/__/___/_//_/_/  \__/
 #
 # ------------------------------------------------------------- // shell prompt
-BG_TIME=$(theme_color black)
-BG_USER=$(theme_color bright_black)
-BG_DIR=$(theme_color magenta)
-FG_DIR=$(theme_color fg)
+FG_DIR=$(theme_color bright_magenta)
 FG_VENV=$(theme_color bright_cyan)
 FG_GIT=$(theme_color white)
-FG_GLYPH=$(theme_color cyan)
+FG_GLYPH=$(theme_color magenta)
 NEWLINE=$'\n'
 
 # helper for colors
@@ -39,17 +36,15 @@ venv_info() {
 set_prompt() {
   local hostname prompt_user current_dir fun_glyph venv gitinfo
 
-  hostname="%K{$BG_TIME} $HOSTNAME %k"
-  prompt_user="%K{$BG_USER} %n %k"
-  current_dir="%K{$BG_DIR} %F{$FG_DIR}%~%f %k"
   venv="%F$(venv_info)"
-  gitinfo="$(git_info)"
-  fun_glyph="%F{$FG_GLYPH}  %f"
+  current_dir="%F{$FG_DIR}%~%f"
+  gitinfo=" $(git_info)"
+  fun_glyph=" %F{$FG_GLYPH}%f "
 
-  PROMPT="${NEWLINE}${hostname}${prompt_user}${current_dir} ${venv}${gitinfo}${fun_glyph}"
+  PROMPT="${venv}${current_dir}${gitinfo}${fun_glyph}"
 }
 
 precmd_functions+=(set_prompt)
 
 # Host info banner (runs once)
-echo -e "\e[1;40m $(hostname) \e[0m\e[1;46m\e[30m $(uptime -p | cut -c 4-) \e[0m \e[0m\e[1;36m$(uname -r)\e[0m"
+echo -e "\e[1;40m $(hostname) \e[0m\e[1;46m\e[30m $(uptime -p | cut -c 4-) \e[0m \e[0m\e[1;36m$(uname -r)\e[0m $NEWLINE"
