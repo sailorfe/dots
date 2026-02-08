376520d203f5346966b260e5da3d7d85b23f1ac7
sailorfe <sailorfe@proton.me>
2026-02-08 14:34:28 -0500

    nvim: lsp/mason comments

--- shell/nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ shell/nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -4,12 +4,15 @@ return {
 		"BufReadPre",
 		"BufNewFile"
 	},
+	-- mason
 	vim.lsp.enable('bashls'),
-	vim.lsp.enable('html'),
 	vim.lsp.enable('lua_ls'),
 	vim.lsp.enable('marksman'),
-	vim.lsp.enable('ty'),
-	vim.lsp.enable('somesass_ls'),
+	vim.lsp.enable('html'),
 	vim.lsp.enable('gopls'),
 	vim.lsp.enable('clangd'),
+	vim.lsp.enable('somesass_ls'),
+	-- uv tool
+	vim.lsp.enable('ty'),
+	vim.lsp.enable('ruff')
 }
