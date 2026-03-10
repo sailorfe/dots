ba522d13e7767c9e258bf46cf352c087ce56294a
sailorfe <sailorfe@noreply.codeberg.org>
2026-03-10 17:10:32 -0400

    gui(wezterm): add envsubst

--- wezterm/.config/wezterm/template.lua
+++ wezterm/.config/wezterm/template.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.font_size = 11
 config.font = wezterm.font('Rec Mono Casual')
-config.color_scheme = 'Luna'
+config.color_scheme = '${THEME_ALIAS}'
 config.use_fancy_tab_bar = false
 config.window_background_opacity = 1.0
 config.initial_cols = 120
