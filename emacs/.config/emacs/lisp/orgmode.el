350d87b5e07df0f80881d3f270b8b4abc5c1960f
sailorfe <sailorfe@noreply.codeberg.org>
2026-08-02 22:45:28 -0400

    emacs(org): fix keybind

--- emacs/.config/emacs/lisp/orgmode.el
+++ emacs/.config/emacs/lisp/orgmode.el
@@ -19,8 +19,8 @@
         '((org-agenda-files :maxlevel . 3))))
 
 ;; keybindings
-(global-set-key (kbd "C-c a") 'org-agenda))
-(global-set-key (kbd "C-c c") 'org-capture))
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
 
 (setq org-log-done 'time)
 
